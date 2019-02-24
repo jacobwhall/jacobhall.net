ce15b976f57d5fc1e2f0a48fd008473900e452cb
Jacob Hall <jwhall@email.wm.edu>
2019-02-23 21:05:38 -0500

    bit of a homepage redesign

---
+++ printkey/phptest.php
@@ -0,0 +1,14 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>My PHP Website</title>
+</head>
+
+<body>
+  <h1>My PHP Website</h1>
+  <p>Here is some static content.</p>
+  <p><?php echo "Here is some dynamic content"; ?></p>
+</body>
+</html>

bfb2c1392c54050f15baac533f8d35ebe94b3f1f
Jacob Hall <email@jacobhall.net>
2020-06-07 17:23:34 -0400

    slightly improved error management in generate.pl

--- generate.pl
+++ generate.pl
@@ -15,7 +15,14 @@ foreach (@dirs) {
 		my $year = $1;
 		my $name = $2;
 		# Try to read article file. If fails, throw error
-		print 'Skipping '.$year.'/'.$name.': no article file' unless $article = read_file($year.'/'.$name.'/'.$name.'.html');
+		if (-e $year.'/'.$name.'/'.$name.'.html') {
+			$article = read_file($year.'/'.$name.'/'.$name.'.html');
+		} else {
+			print 'Skipping '.$year.'/'.$name.": no article file\n";
+			next;
+		}
+		# Try to detect if there is accidentally HTML header code in the article
+		print "WARNING: ".$year.'/'.$name." article file appears to contain unwanted HTML code\n" if ($article =~ /<head>/ || $article =~ /<\/body>/);
 		# Grab title of article
 		$title = read_file($_);
 		# Remove newline in title
@@ -23,12 +30,13 @@ foreach (@dirs) {
 		# Put the title between the <title> tags
 		$beginning =~ s/(?<=<title>).*(?=<\/title>)/$title/;
 		# If there is a file containing additions to <head>
-#		if (-e $year.'/'.'$name'.'/addtohead.html') {
+		if (-e $year.'/'.'$name'.'/addtohead.html') {
+			print 'this folder has an addtohead.html: '.$year.'/'.$name;
 			# Read addtohead.html file and write it to $addtohead
 #			$addtohead = $read_file($year.'/'.'$name'.'/addtohead.html');
 			# Write $addtohead to $beginning before the closing </head> tag
 #			$beginning =~ s/.*(?=<\/head>)/$addtohead/;
-#		}
+		}
 		# Write the concatenated $beginning, $article, and end tags to index.php
 		overwrite_file($year.'/'.$name.'/index.html', $beginning.$article."</body>\n</html>");
 	}	

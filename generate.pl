756de8e9ce6cbfcd3f6ce95a68aa2ff19701510c
Jacob Hall <jwhall@email.wm.edu>
2020-06-07 17:27:35 -0400

    adding ability to load custom code to <head> per-project by adding addtohead.html to project folder

--- generate.pl
+++ generate.pl
@@ -30,12 +30,11 @@ foreach (@dirs) {
 		# Put the title between the <title> tags
 		$beginning =~ s/(?<=<title>).*(?=<\/title>)/$title/;
 		# If there is a file containing additions to <head>
-		if (-e $year.'/'.'$name'.'/addtohead.html') {
-			print 'this folder has an addtohead.html: '.$year.'/'.$name;
+		if (-e $year.'/'.$name.'/addtohead.html') {
 			# Read addtohead.html file and write it to $addtohead
-#			$addtohead = $read_file($year.'/'.'$name'.'/addtohead.html');
+			$addtohead = read_file($year.'/'.$name.'/addtohead.html');
 			# Write $addtohead to $beginning before the closing </head> tag
-#			$beginning =~ s/.*(?=<\/head>)/$addtohead/;
+			$beginning =~ s/.*(?=<\/head>)/$addtohead/;
 		}
 		# Write the concatenated $beginning, $article, and end tags to index.php
 		overwrite_file($year.'/'.$name.'/index.html', $beginning.$article."</body>\n</html>");

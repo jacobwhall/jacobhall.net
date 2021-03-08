6c4d14c8ae46c32b1c4963cbcc6794a0e29469bb
Jacob Hall <email@jacobhall.net>
2021-03-08 15:49:18 -0500

    Work on 2021/desklight and adding a couple h-entry tags to generate.pl

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,6 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'desklight\\' => array($baseDir . '/2021/desklight'),
     'Symfony\\Polyfill\\Php80\\' => array($vendorDir . '/symfony/polyfill-php80'),
     'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
     'Symfony\\Component\\Routing\\' => array($vendorDir . '/symfony/routing'),

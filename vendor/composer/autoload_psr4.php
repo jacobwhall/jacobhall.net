b2b62d962c166677b281dddbc5fc07991b773d7b
Jacob Hall <jwhall@email.wm.edu>
2021-04-04 16:02:29 -0400

    simple micropub endpoint is workingls!

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,7 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'webmentions\\' => array($baseDir . '/webmentions'),
+    'micropub\\' => array($baseDir . '/micropub'),
     'Symfony\\Polyfill\\Php80\\' => array($vendorDir . '/symfony/polyfill-php80'),
     'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
     'Symfony\\Component\\Routing\\' => array($vendorDir . '/symfony/routing'),

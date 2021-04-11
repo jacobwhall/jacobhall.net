640333bcb25c6ea311ec7cd037f504c83b152fc7
Jacob Hall <email@jacobhall.net>
2021-04-11 12:08:35 -0400

    added delete and undelete actions to micropub server

--- indieweb/micropub.php
+++ indieweb/micropub.php
@@ -137,24 +137,117 @@ function make_html($input) {
 // We'll be better about handling photos another day :)
 $photo_urls = array();
 
+function normalize_properties($properties) {
+    $props = [];
+    foreach ($properties as $k => $v) {
+        # we want the "photo" property to be an array, even if it's a
+        # single element.  Our Hugo templates require this.
+        if ($k == 'photo') {
+            $props[$k] = $v;
+        } elseif (is_array($v) && count($v) === 1) {
+            $props[$k] = $v[0];
+        } else {
+            $props[$k] = $v;
+        }
+    }
+    return $props;
+}
+
+
+// Much of this code borrowed from https://github.com/skpy/micropub/blob/master/inc/content.php
+// this probably belongs in the micropub file tbh
+function create($request, $photos = []) {
+
+	$mf2 = $request->toMf2();
+	# make a more normal PHP array from the MF2 JSON array
+	$properties = normalize_properties($mf2['properties']);
+
+	# pull out just the content, so that $properties can be front matter
+	# NOTE: content may be in ['content'] or ['content']['html'].
+	# NOTE 2: there may be NO content!
+	if (isset($properties['content'])) {
+		if (is_array($properties['content']) && isset($properties['content']['html'])) {
+			$content = $properties['content']['html'];
+		} else {
+			$content = make_html($properties['content']);
+		}
+	} else {
+		$content = '';
+	}
+	# ensure that the properties array doesn't contain 'content'
+	unset($properties['content']);
+
+	if (!empty($photos)) {
+	# add uploaded photos to the front matter.
+		if (!isset($properties['photo'])) {
+			$properties['photo'] = $photos;
+		} else {
+			not_implemented("photo uploads");
+			// $properties['photo'] = array_merge($properties['photo'], $photos);
+		}
+	}
+	if (!empty($properties['photo'])) {
+	//	$properties['thumbnail'] = preg_replace('#-' . $config['max_image_width'] . '\.#', '-200.', $properties['photo']);
+	}
+
+	# figure out what kind of post this is.
+	$properties['posttype'] = post_type_discovery($properties);
+	// echo ("post type is: ". $properties['posttype']);
+	
+	if (isset($properties['post-status'])) {
+		if ($properties['post-status'] == 'draft') {
+			$properties['published'] = false;
+		} else {
+			$properties['published'] = true;
+		}
+		unset($properties['post-status']);
+	} else {
+		# explicitly mark this item as published
+		$properties['published'] = true;
+	}
+
+	// echo ("published: ". $properties['published']);
+	$insertionData = [
+		"posttype" => $properties['posttype'],
+		"posttitle" => NULL,
+		"content" => $content,
+		"published" => $properties['published'],
+		"bookmarkof" => NULL,
+	];
+	if (isset($properties["category"])) {
+		if (is_array($properties["category"])) {
+			$categories = $properties["category"];
+		} else {
+			$categories = array();
+			array_push($categories, $properties["category"]);
+		}
+	} else {
+		$categories = array();
+	}
+
+	if (isset($properties["bookmark-of"])) {
+		$insertionData["bookmarkof"] = $properties["bookmark-of"];
+	}
+	if (isset($properties["name"])) {
+		$insertionData["posttitle"] = $properties["name"];
+	}
+	
+	insertPost($insertionData, $categories);
+}
+
 switch($request->action):
 	case 'delete':
-		not_implemented($request->action);
-		// delete($request);
+		setPublishedPost($request->url, FALSE);
 		break;
 	case 'undelete':
-		not_implemented($request->action);
-		// undelete($request);
+		setPublishedPost($request->url, TRUE);
 		break;
 	case 'update':
 		not_implemented($request->action);
 		// update($request, $photo_urls);
 		break;
 	default:
-		//not_implemented($request->action);
 		create($request, $photo_urls);
 		break;
 	endswitch;
-
-
 ?>

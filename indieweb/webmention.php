a858abfa809cbe8b2e84c8d97c2db35005af5f5c
Jacob Hall <email@jacobhall.net>
2021-08-07 17:58:35 -0400

    log webmention post microformats as JSON to wm_log table

--- indieweb/webmention.php
+++ indieweb/webmention.php
@@ -3,20 +3,30 @@
 require __DIR__ . '/vendor/autoload.php';
 include_once './postinsertion.php';
 
+$commentInserted = False;
+
 if (!isset($_POST['source']) || !isset($_POST['target'])) {
 	header($_SERVER['SERVER_PROTOCOL'] . ' 400 Bad Request');
-	echo "hi there, to send me a webmention you'll have to post a source and target url to your request. \nif that makes no sense, shoot me an email at email@jacobhall.net and i'd be happy to explain it to you :) \ni hope you have a great day!\n";
+	echo "Hi there, to send me a webmention you'll have to post a source and target url to your request. \nif that makes no sense, shoot me an email at email@jacobhall.net and i'd be happy to explain it to you :) \ni hope you have a great day!\n";
   exit;
 } elseif (parse_url($_POST['target'])['host'] != "jacobhall.net") {
 	header($_SERVER['SERVER_PROTOCOL'] . ' 400 Bad Request');
-	echo "it doesn't look like your target URL is to my website, jacobhall.net!\n";
-} else {
+	echo "It doesn't look like your target URL is to my website, jacobhall.net!\n";
+}
+
+
+function insertComment($source, $target, $whostyle = Null, $item = Null) {
+	$item = json_encode($item);
+	global $commentInserted;
+	$commentInserted = True;
 	// Log this webmention request
 	$conn = connectToDB();
-	$logquery = "INSERT INTO wm_log (source, target) VALUES (:source, :target)";
+	$logquery = "INSERT INTO wm_log (source, target, whostyle, source_mf) VALUES (:source, :target, :whostyle, :source_mf)";
 	$lqp = $conn->prepare($logquery);
-	$lqp->bindParam(":source", $_POST["source"]);
-	$lqp->bindParam(":target", $_POST["target"]);
+	$lqp->bindParam(":source", $source);
+	$lqp->bindParam(":target", $target);
+	$lqp->bindParam(":whostyle", $whostyle);
+	$lqp->bindParam(":source_mf", $item);
 	if (!$lqp->execute($logData)) {
 		header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error');
 		echo "Request logging unsuccessful! Please contact the webmaster.";
@@ -25,30 +35,6 @@ if (!isset($_POST['source']) || !isset($_POST['target'])) {
 		print_r($arr);
 		exit;
 	}
-	
-	// Create contacts to save array
-	$contactsToSave = array();
-}
-
-
-function insertCommentProperties($propertyarray) {
-	// echo "comment property array passed!\n";
-	
-	// THIS FUNCTION IS TODO !!!!
-	// THIS FUNCTION WILL CONVERT THE PASSED $propertyarray
-	// into the $insertionData array insertPost() needs to
-	// insert the post into the database!!
-	
-	// echo ("published: ". $properties['published']);
-	$insertionData = [
-		//"posttype" => $properties['posttype'],
-		"posttitle" => NULL,
-		//"content" => $content,
-		//"published" => $properties['published'],
-		"bookmarkof" => NULL,
-	];
-
-	//insertPost("to_mod", $insertionData, []);
 }
 
 function processHCard($url) {
@@ -71,7 +57,7 @@ function sniffForHCards($propertyarray) {
 	}
 }
 
-$replytothis = False;
+$isReply = False;
 // Get microformats array of the source
 $sourcemf = mf2\fetch($_POST['source']);
 foreach ($sourcemf[items] as $item) {
@@ -86,13 +72,14 @@ foreach ($sourcemf[items] as $item) {
 			foreach ($item['properties']['in-reply-to'] as $replyitem) {
 				// If this post replies to the target (my post)
 				if ($replyitem['properties']['url'][0] == $_POST['target']) {
-					$replytothis = True;
+					$isReply = True;
 				}
 				// Sniff for h-cards to add to my contacts
 				sniffForHCards($replyitem['properties']);
 			}
-			if ($replytothis) {
-				insertCommentProperties($item['properties']);
+			if ($isReply) {
+				// TODO: support whostyle sniffing
+				insertComment($_POST["source"], $_POST["target"], Null, $item);
 			}
 		}
 	} else {
@@ -105,14 +92,15 @@ foreach ($sourcemf[items] as $item) {
 }
 
 // Is the target URL even in the source?
-if ($replytothis) {
+if ($commentInserted) {
 	header($_SERVER['SERVER_PROTOCOL'] . ' 202 Accepted');
 	
 	echo "Successfully received and validated your webmention!\nJust a heads up that I am moderating these manually, so if you want your comment to appear quickly do be in touch!";
 	// print_r($contactsToSave);
 } else {
+	insertComment($_POST["source"], $_POST["target"]);
 	header($_SERVER['SERVER_PROTOCOL'] . ' 400 Bad Request');
-	echo "thanks, but the source does not seem to refer to the target! rejecting your webmention :(\n";
+	echo "Thanks for the webmention!\nMy system reports that your post might not link to mine, though I will check that manually.\n";
 }
 
 ?>

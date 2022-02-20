9176d344e0538df5649ab17dfe6e3c1e020f34fa
Jacob Hall <email@jacobhall.net>
2022-02-20 02:23:08 -0500

    improving dispatcher formatting and post formatting
    - dispatchers are now sequenced, applied in order of priority
    - there are now reusable templates for posts and articles

--- site.rkt
+++ site.rkt
@@ -1,24 +1,43 @@
 #lang racket
 
+; Thanks to Bogdan Popa for their helpful guide to racket's web server
+; https://defn.io/2020/02/12/racket-web-server-guide/
+
 (require db
-         web-server/dispatch
-         web-server/servlet
+         net/url
          web-server/servlet-env
+         web-server/http
          web-server/templates
+         web-server/web-server
+         web-server/servlet-dispatch
+         web-server/dispatch
+         web-server/dispatchers/filesystem-map
+         (prefix-in sequencer: web-server/dispatchers/dispatch-sequencer)
+         (prefix-in files: web-server/dispatchers/dispatch-files)
          "../pass.rkt")
+
+(define (http-response content)
+  (response/full
+    200                  ; HTTP response code.
+    #"OK"                ; HTTP response message.
+    (current-seconds)    ; Timestamp.
+    TEXT/HTML-MIME-TYPE  ; MIME type for content.
+    '()                  ; Additional HTTP headers.
+    (list                ; Content (in bytes) to send to the browser.
+      (string->bytes/utf-8 content))))
 
 (define (a key post)
   (cdr (assoc key post)))
 
 (define (row->ass headers row)
-  (map (lambda (h r) make-hash (cons (cdr (first h)) (if (string? r) r ""))) headers (sequence->list row)))
+  (map (λ (h r) make-hash (cons (cdr (first h)) (if (sql-null? r) "" r))) headers (sequence->list row)))
 
 (define (ass->post post-data)
   (include-template "post.html"))
 
 (define (rows-result->posts result)
-  (foldl string-append ""
-         (map (lambda (r) (ass->post (row->ass (rows-result-headers result) r))) (rows-result-rows result))))
+  (foldr string-append ""
+         (map (λ (r) (ass->post (row->ass (rows-result-headers result) r))) (rows-result-rows result))))
 
 (define posts (rows-result->posts (query pgc "SELECT *
 		FROM vPosts
@@ -31,20 +50,30 @@
 (define (homepage req)
   (http-response (include-template "index.html")))
 
+(define (article content)
+  (http-response (include-template "article.html")))
 
-(define (http-response content)
-  (response/full
-    200                  ; HTTP response code.
-    #"OK"                ; HTTP response message.
-    (current-seconds)    ; Timestamp.
-    TEXT/HTML-MIME-TYPE  ; MIME type for content.
-    '()                  ; Additional HTTP headers.
-    (list                ; Content (in bytes) to send to the browser.
-      (string->bytes/utf-8 content))))
+(define (links req)
+  (http-response (port->string (open-input-file "links.html"))))
 
-(serve/servlet;
- homepage
- #:servlet-path "/"
- #:extra-files-paths
- (list
-  (build-path "/home/jacob/Documents/jacobhall.net")))
+(define (not-found req)
+  (article "<h1>Not Found</h1>")) ; TODO: use xexpr
+
+(define-values (main-dispatcher _)
+  (dispatch-rules
+   [("") homepage]
+   [("kind") (λ (r) (article posts))]
+   [("links") (λ (r) (article (file->string "links.html")))]
+   [("dreams") (λ (r) (article (file->string "dreams.html")))]))
+
+(define stop
+  (serve
+   #:dispatch (sequencer:make (dispatch/servlet main-dispatcher)
+                              (files:make #:url->path (make-url->path "."))
+                              (dispatch/servlet not-found))
+   #:listen-ip "127.0.0.1"
+   #:port 8000))
+
+(with-handlers ([exn:break? (λ (e)
+                              (stop))])
+  (sync/enable-break never-evt))

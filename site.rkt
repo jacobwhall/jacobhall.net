68fbeb03be06423e2f86d269695b542aafb0d254
Jacob Hall <email@jacobhall.net>
2022-02-21 22:25:27 -0500

    database querying improvements, general cleanup
    - statements now get prepared, with optional arguments
    - posts now have human-readable date values, courtesy of an
      updated PostgreSQL view
    - articles now have titles
    - cleanup

--- site.rkt
+++ site.rkt
@@ -30,41 +30,67 @@
   (cdr (assoc key post)))
 
 (define (row->ass headers row)
-  (map (λ (h r) make-hash (cons (cdr (first h)) (if (sql-null? r) "" r))) headers (sequence->list row)))
+  (map (λ (h r)
+         make-hash
+         (cons (cdr (first h))
+               (if (sql-null? r)
+                   ""
+                   r)))
+       headers (sequence->list row)))
 
 (define (ass->post post-data)
   (include-template "post.html"))
 
 (define (rows-result->posts result)
   (foldr string-append ""
-         (map (λ (r) (ass->post (row->ass (rows-result-headers result) r))) (rows-result-rows result))))
+         (map (λ (r)
+                (ass->post (row->ass (rows-result-headers result)
+                                     r)))
+              (rows-result-rows result))))
 
-(define posts (rows-result->posts (query pgc "SELECT *
-		FROM vPosts
-		WHERE type != 5
-		AND type != 6
-		AND type != 11
-		AND author = 'Jacob Hall'
-                LIMIT 5")))
+(define posts
+  (λ (limit
+      #:author [author "Jacob Hall"]
+      #:types [types (list 1 2 3 4 5 6 7 8 9 10 11 12)])
+    (rows-result->posts
+                       (query
+                        pgc
+                        (prepare pgc "SELECT *
+                                      FROM vPosts
+                                      WHERE author = $2
+                                      AND type=ANY($3::int[])
+                                      LIMIT $1")
+                        limit
+                        author
+                        types))))
 
 (define (homepage req)
   (http-response (include-template "index.html")))
 
-(define (article content)
+(define (article title content)
   (http-response (include-template "article.html")))
 
 (define (links req)
   (http-response (port->string (open-input-file "links.html"))))
 
 (define (not-found req)
-  (article "<h1>Not Found</h1>")) ; TODO: use xexpr
+  (article "404" "<h1>Not Found</h1>")) ; TODO: use xexpr
 
 (define-values (main-dispatcher _)
   (dispatch-rules
    [("") homepage]
-   [("kind") (λ (r) (article posts))]
-   [("links") (λ (r) (article (file->string "links.html")))]
-   [("dreams") (λ (r) (article (file->string "dreams.html")))]))
+   [("kind") (λ (r)
+               (article "all posts"
+                        (posts 25)))]
+   [("about") (λ (r)
+                (article "about"
+                         (file->string "about.html")))]
+   [("links") (λ (r)
+                (article "links"
+                         (file->string "links.html")))]
+   [("dreams") (λ (r)
+                 (article "dreams"
+                          (file->string "dreams.html")))]))
 
 (define stop
   (serve
